67c8b9eb5294e1e8c7a28f572f97d7f05ba4f459
Oleksandr-2022 <zibyuk.2017@gmail.com>
2022-11-06 19:42:25 +0200

    06/11

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,34 +1,58 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-
 const iframe = document.querySelector('iframe');
-
 const LOCAL_STORAGE_PLAY_TIME = 'videoplayer-current-time';
-
-const lastPlayTime = localStorage.getItem(LOCAL_STORAGE_PLAY_TIME) ?? 0;
-
 const player = new Player(iframe);
 
-const updateTimePlay = throttle(function (data) {
-  localStorage.setItem(LOCAL_STORAGE_PLAY_TIME, data.seconds);
-}, 1000);
+// player.on('timeupdate', throttle(onSetTimeupdate, 500));
+function onSetTimeupdate(data) {
+    console.log('data.seconds: ', data.seconds);
+    localStorage.setItem(LOCAL_STORAGE_PLAY_TIME, data.seconds);
+  }
+  
+  if (localStorage.getItem(LOCAL_STORAGE_PLAY_TIME)) {
+    player.setCurrentTime(localStorage.getItem(LOCAL_STORAGE_PLAY_TIME));
+  }
 
-player.on('timeupdate', updateTimePlay);
 
-player
-  .setCurrentTime(lastPlayTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
 
+// // const updateTimePlay = throttle(function (data) {
+// //   localStorage.setItem(LOCAL_STORAGE_PLAY_TIME, data.seconds);
+// // }, 1000);
+
+// // player.on('timeupdate', updateTimePlay);
+
+// // player
+// //   .setCurrentTime(lastPlayTime)
+// //   .then(function (seconds) {
+// //     // seconds = the actual time that the player seeked to
+// //   })
+// //   .catch(function (error) {
+// //     switch (error.name) {
+// //       case 'RangeError':
+// //         // the time was less than 0 or greater than the video’s duration
+// //         break;
+
+// //       default:
+// //         // some other error occurred
+// //         break;
+// //     }
+// //   });
+
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// const keyCurrentTime = 'videoplayer-current-time';
+
+// player.on('timeupdate', throttle(onSetTimeupdate, 500));
+// // function onSetTimeupdate(data) {
+// //   console.log('data.seconds: ', data.seconds);
+// //   localStorage.setItem(keyCurrentTime, data.seconds);
+// // }
+
+// // if (localStorage.getItem(keyCurrentTime)) {
+// //   player.setCurrentTime(localStorage.getItem(keyCurrentTime));
+// // }
